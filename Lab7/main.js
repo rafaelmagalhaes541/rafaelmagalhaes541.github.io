7332399a4b383746351c9c6de8688f6fff8e9761
rafaelmagalhaes <a22407372@alunos.ulht.pt>
2025-10-31 16:03:55 +0000

    Lab 6 concluido

---
+++ Lab7/main.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", () => {
+    carregarProdutos(produtos);
+    mostrarCarrinho(); 
+});
+
+function carregarProdutos(produtos) {
+    const secaoProdutos = document.querySelector("#produtos"); 
+
+    produtos.forEach(produto => {
+        const artigo = criarProduto(produto);
+        secaoProdutos.appendChild(artigo);
+    });
+}
+
+function criarProduto(produto) {
+    const artigo = document.createElement("article");
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = produto.title;
+
+    const imagem = document.createElement("img");
+    imagem.src = produto.image;
+    imagem.alt = produto.title;
+
+    const descricao = document.createElement("p");
+    descricao.textContent = produto.description;
+
+    const preco = document.createElement("p");
+    preco.textContent = `Preço: €${produto.price}`;
+
+    const botao = document.createElement("button");
+    botao.textContent = "Adicionar ao Carrinho";
+    botao.addEventListener("click", () => adicionarAoCarrinho(produto));
+
+    artigo.appendChild(titulo);
+    artigo.appendChild(imagem);
+    artigo.appendChild(descricao);
+    artigo.appendChild(preco);
+    artigo.appendChild(botao);
+
+    return artigo;
+}
+
+function adicionarAoCarrinho(produto) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    carrinho.push(produto);
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+    mostrarCarrinho();
+}
+
+function mostrarCarrinho() {
+    const idPreco = document.querySelector("#preco")
+    const secCarrinho = document.querySelector("#carrinho");
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+    secCarrinho.innerHTML = "";
+
+    carrinho.forEach((p, index) => { 
+        const item = document.createElement("article");
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = p.title;
+
+        const imagem = document.createElement("img");
+        imagem.src = p.image;
+        imagem.alt = p.title;
+
+        const preco = document.createElement("p");
+        preco.textContent = `Preço: €${p.price}`;
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "Remover do Carrinho";
+        botaoRemover.addEventListener("click", () => removerDoCarrinho(index));
+
+        item.append(titulo, imagem, preco, botaoRemover);
+        secCarrinho.append(item);
+    });
+
+    idPreco.innerHTML = ""
+    
+    const total = carrinho.reduce((soma, p) => soma + p.price, 0);
+    const totalEl = document.createElement("p");
+    totalEl.textContent = `Total: €${total}`;
+    idPreco.append(totalEl);
+}
+
+function removerDoCarrinho(index) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    carrinho.splice(index, 1);
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    mostrarCarrinho();
+}
